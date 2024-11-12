4d1616bb3c90a84cbcb56df52dae7802739ae1bd
Befich <samuelcortez454@gmail.com>
2024-11-12 15:24:23 -0300

    Update codigbrasil.py

--- codigbrasil.py
+++ codigbrasil.py
@@ -2,5 +2,15 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-st.write("""alo
+st.write("""alo?
 """)
+st.markdown("""
+<style>
+    [data-testid=stSidebar] {
+        background-color: #9c9c9c;
+    }
+</style>
+""", unsafe_allow_html=True)
+
+with st.sidebar:
+    "## This is the sidebar"
